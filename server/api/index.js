bb19c8531d003f9254f48b80d27ba0534cbae313
David Udvardy <udvardy@gmail.com>
2018-10-25 00:29:23 +0200

    creating new job story backend is ready

--- server/api/index.js
+++ server/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 router.use(require('./categories'))
 router.use(require('./jobstories'))
 router.use(require('./jobstory-update'))
+router.use(require('./jobstory-add'))
 
 module.exports = router
--- server/api/jobstory-update.js
+++ server/api/jobstory-update.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { db } = require('../lib/database')
 
 var bodyParser = require('body-parser')
+router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
 // Update job story with jobId passed in path
@@ -12,7 +13,7 @@ router.put('/api/jobstory-update/:jobId', (req, res) => {
             return t.oneOrNone(`SELECT context_id, motivation_id, outcome_id FROM jobstories WHERE id = $1;`, req.params.jobId)
                 .then(data => {
                     if (data) {
-                        return t.multi(`UPDATE contexts SET description=$2 WHERE id = $1; UPDATE motivations SET description=$4 WHERE id = $3; UPDATE outcomes SET description=$6 WHERE id = $5;`, [
+                        return t.multi(`UPDATE contexts SET description=$2 WHERE id = $1 RETURNING id,description; UPDATE motivations SET description=$4 WHERE id = $3 RETURNING id,description; UPDATE outcomes SET description=$6 WHERE id = $5 RETURNING id,description;`, [
                             data.context_id,
                             req.body.context,
                             data.motivation_id,
@@ -26,7 +27,7 @@ router.put('/api/jobstory-update/:jobId', (req, res) => {
         })
         .then(data => {
             res.send(data);
-            console.log('Response:', data);
+            console.log('Updated', data.length, 'tables with data:', data);
         })
         .catch(function (err) {
             res.json(`Couldn't serve ${req.path} request. Error message: ${JSON.stringify(err)}`)
