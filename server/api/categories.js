010c3d3e80a64f9f1ffe4fa3dd0eb7a7f94b10f3
David Udvardy <udvardy@gmail.com>
2018-12-12 23:24:12 +0100

    product titles are sorted alphabetically in sidebar

--- server/api/categories.js
+++ server/api/categories.js
@@ -6,7 +6,7 @@ const { db } = require('../lib/database')
 // Serve categories from API
 router.get("/api/categories", (req, res) => {
     db
-        .any(`SELECT * FROM (SELECT p.id AS key, p.title, (SELECT array_to_json(array_agg(row_to_json(usertypes))) FROM (SELECT u.id AS key, u.title FROM usertypes AS u WHERE u.product_id=p.id ORDER BY u.title) usertypes) as usertypes FROM products AS p) product_usertypes;`)
+        .any(`SELECT * FROM (SELECT p.id AS key, p.title, (SELECT array_to_json(array_agg(row_to_json(usertypes))) FROM (SELECT u.id AS key, u.title FROM usertypes AS u WHERE u.product_id=p.id ORDER BY u.title) usertypes) as usertypes FROM products AS p ORDER BY p.title) product_usertypes;`)
         .then(data => {
             res.send(data);
             console.log('︎︎︎︎︎︎Success: Sent', data.length, 'product categories.');
